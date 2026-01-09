651d70cda7b229b52124b4b35acd768286847b43
Oleksandr Hrymalovskyi <grimalsan@ukr.net>
2026-01-09 16:57:28 +0100

    Добавил company details API и путь / для главной страницы

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -115,3 +115,8 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ────── Company Info ──────
+COMPANY_NAME = 'StrideWay'
+SLOGAN = 'Your path. Your pace. Your progress.'
+CONTACTS = 'Diblerstasse 12a, 10115 Berlin, Germany'
