217694067236e372fde3f48d8cabbd241a813916
Oleksandr Hrymalovskyi <grimalsan@ukr.net>
2026-01-09 17:09:29 +0100

    Добавил company details API и путь / для главной страницы + исправление

--- app_run/views.py
+++ app_run/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 @api_view(['GET'])
 def company_details(request):
-    details = {'compani_name': settings.COMPANY_NAME,
+    details = {'company_name': settings.COMPANY_NAME,
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
