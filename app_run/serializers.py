2aadc313f1f27db506f6aef5c712c15e75b24da8
Oleksandr Hrymalovskyi <grimalsan@ukr.net>
2026-01-24 12:39:58 +0100

    Добавлен RunSerializer с athlete_data и оптимизация N+1, обновлён views.py и urls.py

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -4,9 +4,21 @@ from django.contrib.auth.models import User
 
 
 class RunSerializer(serializers.ModelSerializer):
+    athlete_data = serializers.SerializerMethodField() 
     class Meta:
         model = Run
         fields = '__all__'
+        
+    def get_athlete_data(self, obj):
+        user = obj.athlete
+        return {
+            'id' : user.id,
+            'username' : user.username,
+            'last_name' : user.last_name,
+            'first_name' : user.first_name
+        }
+        
+        
         
         
 class UserSerializer(serializers.ModelSerializer):
